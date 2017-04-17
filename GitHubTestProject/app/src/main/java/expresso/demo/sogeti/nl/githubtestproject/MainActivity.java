5c42c7478fe9683d355f7510b3599c315648d589
Sourav <sourav.borphukon@philips.com>
2017-04-17 17:08:41 +0200

    First commit

---
+++ GitHubTestProject/app/src/main/java/expresso/demo/sogeti/nl/githubtestproject/MainActivity.java
@@ -0,0 +1,13 @@
+package expresso.demo.sogeti.nl.githubtestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
